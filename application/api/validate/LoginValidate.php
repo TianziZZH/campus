8112a8023cf048705737d89da8b32ae9bfdf78e4
Tianzi <1551834739@qq.com>
2018-03-26 12:27:04 +0800

    v1.1.5
    修复用户信息不足的bug

--- application/api/validate/LoginValidate.php
+++ application/api/validate/LoginValidate.php
@@ -14,12 +14,16 @@ class LoginValidate extends BaseValidate
     protected $rule = [
         'userid' => 'require',
         'password' => 'require',
-        'school' => 'require|isPositiveInteger'
+        'school' => 'require|isPositiveInteger',
+        'nickname' => 'require',
+        'avatar' => 'require'
     ];
 
     protected $message = [
         'userid' => '学号不能为空',
         'password' => '密码不能为空',
         'school' => '学校选择不能为空',
+        'nickname' => '微信名不能为空',
+        'avatar' => '微信头像不能为空'
     ];
 }
